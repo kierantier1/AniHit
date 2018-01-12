d37a763074af6108b5612e886ea74cb2ec40b749
hallk1331 <hallk1331@FHC-IC0034079.wrdsb.ca>
2018-01-12 14:15:18 -0500

    Initial Commit

---
+++ desktop/src/gdx/anihit/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package gdx.anihit.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import gdx.anihit.AniHit;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AniHit(), config);
+	}
+}
